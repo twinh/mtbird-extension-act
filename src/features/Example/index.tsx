a02ab93706c9e5e27f13453388689e3d83fb0a7f
twinh <twinhuang@qq.com>
2023-03-22 14:10:35 +0800

    feat: init

---
+++ src/features/Example/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const Example = () => {
+  return (
+    <div>
+      MTBIRD <Button>go home 123</Button>
+    </div>
+  );
+};
+
+export default Example;
